eccd9a486ceb3dd4c77a286d1dda33016065bfe2
dianavashti <diana.vashti@gmail.com>
2018-01-10 00:19:16 -0800

    Solves most specs, complete feature and add new todo form feature not yet implemented

--- public/script.js
+++ public/script.js
@@ -1,8 +1,43 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function() {
-  getAllTodos()
-  // add rest of code here
+  getAllTodos();
+
+  // becasue the delete-btn is added dynamically
+  // the click handler needs to be written like such, bound to the document
+  $(document).on('click', '.delete-btn', function() {
+    var id = $(this).data('itemid');
+    $.ajax({
+      // add code here
+      method: "POST",
+      url: '/ajax/delete/'+id,
+      success: handleTodosDeleteResponse(id)
+    });
+  });
+
+  $(document).on('click', '.edit-btn', function() {
+    var id = $(this).data("itemid")
+    // hide the static item, show the input field
+    $('.item-'+id).hide()
+    $('.input-'+id).show()
+
+    // hide the edit button, show the save button
+    $('.edit-'+id).hide()
+    $('.save-'+id).show()
+
+  })
+
+  $(document).on('click', '.save-btn', function() {
+    var id = $(this).data("itemid")
+    var updatedItem = $('.input-'+id+' input').val()
+
+    $.ajax({
+      method: 'PUT',
+      url: '/ajax/update/'+id,
+      data: {item: updatedItem},
+      success: handleTodosUpdateResponse(id, updatedItem)
+    })
+  })
 
 });
 
@@ -10,19 +45,37 @@ function getAllTodos() {
   $('.list-group').html('')
   $.ajax({
     // add your code here
+    method: 'GET',
+    url: '/ajax/get-all'
   }).done(function(data) {
     for( let i=0; i<data.todos.length; i++){
       let itemId = data.todos[i].id
-      $('.list-group').append(
-        // add rest of code here
-      )}
+      $('.list-group').append('<tr class="list-group-item listitem-'+itemId+'">'
+        +'<td>'
+          +'<button class="btn btn-primary edit-btn edit-'+itemId+'" data-itemid="'+itemId+'">Edit</button>'
+          +'<button class="btn btn-success save-btn save-'+itemId+'" data-itemid="'+itemId+'">Save</button>'
+        +'</td>'
+        +'<td>'
+          +'<span class="item-'+itemId+' item">&nbsp;'+data.todos[i].item+'</span>'
+          +'<span class="form-inline edit-form input-'+itemId+'">&nbsp;<input class="form-control" value="'+data.todos[i].item+'"/></span>'
+        +'</td>'
+        +'<td>'
+          +'<button class="btn btn-danger delete-btn pull-right" data-itemid="'+itemId+'">Delete</button>'
+        +'</td>'
+      +'</tr>') }
   });
 };
 
-function handleTodosDeleteResponse(args) {
-  // manip that DOM
+function handleTodosDeleteResponse(itemId) {
+  var $row = $('.listitem-' + itemId);
+  $row.remove();
 }
 
-function handleTodosUpdateResponse(args) {
-  // manip that DOM
+function handleTodosUpdateResponse(id, updatedItem) {
+  $('.item-'+id).html('&nbsp;'+updatedItem)
+
+  $('.item-'+id).show()
+  $('.input-'+id).hide()
+  $('.edit-'+id).show()
+  $('.save-'+id).hide()
 }
